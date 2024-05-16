d753278a8b21ed2d2dd87fdfaa6d48388addd8c3
Luis Correia <luis.v.correia@tecnico.ulisboa.pt>
2024-05-16 15:04:57 +0100

    Advanced implementation of cylinder
    Co-authored-by: Afonso Ribeiro <ARibeiro3110@users.noreply.github.com>

--- TrabalhoC/js/main-script.js
+++ TrabalhoC/js/main-script.js
@@ -7,20 +7,50 @@ import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 //////////////////////
 /* GLOBAL VARIABLES */
 //////////////////////
-
+var carousel, renderer, camera, scene, controls, clock;
 
 /////////////////////
 /* CREATE SCENE(S) */
 /////////////////////
-function createScene(){
+function createScene() {
     'use strict';
+
+    scene = new THREE.Scene();
+
+    scene.add(new THREE.AxesHelper(10));
+
+    // background color
+    scene.background = new THREE.Color(0xFFFFFF);
+
+    carousel = createCarousel();
 
 }
 
 //////////////////////
+/* GLOBAL CONSTANTS */
+//////////////////////
+const M = Object.freeze({ // Material constants
+    cylinder: new THREE.MeshBasicMaterial({color: 0x00FF00, wireframe: true}),
+    ring1: new THREE.MeshBasicMaterial({color: 0xFF0000, wireframe: false}),
+    ring2: new THREE.MeshBasicMaterial({color: 0x0000FF, wireframe: false}),
+    ring3: new THREE.MeshBasicMaterial({color: 0xFFFF00, wireframe: false})
+    
+});
+
+//////////////////////
 /* CREATE CAMERA(S) */
 //////////////////////
+function createCamera() {
+    'use strict';
 
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.set(20, 20, 20);
+    camera.lookAt(scene.position);
+
+    scene.add(camera);
+
+    controls = new OrbitControls(camera, renderer.domElement);
+}
 
 /////////////////////
 /* CREATE LIGHT(S) */
@@ -29,6 +59,22 @@ function createScene(){
 ////////////////////////
 /* CREATE OBJECT3D(S) */
 ////////////////////////
+function createCarousel() {
+    'use strict';
+
+    var carousel = new THREE.Object3D();
+
+    const cylinder = new THREE.Mesh(new THREE.CylinderGeometry(1, 1, 10, 32), M.cylinder);
+    cylinder.position.set(0, 5, 0);
+
+    // Add cylinder and rings to carousel
+    carousel.add(cylinder);
+
+    scene.add(carousel);
+
+    return carousel;
+
+}
 
 //////////////////////
 /* CHECK COLLISIONS */
@@ -59,7 +105,7 @@ function update(){
 /////////////
 function render() {
     'use strict';
-
+    renderer.render(scene, camera);
 }
 
 ////////////////////////////////
@@ -68,6 +114,17 @@ function render() {
 function init() {
     'use strict';
 
+    renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    clock = new THREE.Clock();
+
+    createScene();
+    createCamera();
+
 }
 
 /////////////////////
@@ -75,6 +132,14 @@ function init() {
 /////////////////////
 function animate() {
     'use strict';
+
+    requestAnimationFrame(animate);
+
+    update();
+
+    render();
+
+    controls.update();
 
 }
 
